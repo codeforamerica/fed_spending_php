a4b5a474e0b04942edf80d9223a09a2d0f331666
Ronaldo Barbachano <ronaldo@redcapmedia.com>
2011-06-27 13:30:53 -0700

    Added files

---
+++ fedSpendingApi.php
@@ -0,0 +1,119 @@
+<?php
+
+class fedSpendingApi extends APIBaseClass{
+// supports XML and HTML
+	public static $api_url = 'http://www.fedspending.org/';
+	
+	public static $api_key = '';
+	
+	public static $base_options = 
+				array('datype' => 'required',
+				'detail'=>array(-1,0,1,2,3,4),
+				'sortby' => array('f','r','g','p','d') ,
+				'max_records' => 999, 		
+				'fiscal_year'=>'',
+				'first_year_range'=>'',
+				'last_year_range'=>'');
+	
+	
+	public function __construct($url=NULL)
+	{
+		parent::new_request(($url?$url:self::$api_url));
+	}
+	
+	public function make_request($options,$method_name){
+		foreach($options as $key=>$value){
+				if((is_array($valid_options[$key]) && in_array($value,$valid_options[$key])) || array_key_exists($key,$valid_options))
+				$data[$key] = $value;
+		}
+		// add datatype - disable this line to return HTML
+		$data['datype'] = 'X';
+		return self::_request($path.$method_name,'get',$data);
+	}
+	
+	public function contracts($options,$detail=-1,$state=NULL){
+	// pass parameters via options, theres a parameter at the 
+	// fpds.php
+	// at least one option is required to search
+	
+		$valid_options =array( 
+				'company_name'=>'',
+				'city'=>'',
+				'state'=>'',
+				'ZIPCode'=>'',
+				'vendorCountryCode'=>'',
+				'vendor_cd'=>'',
+				'pop_cd'=>'',
+				'stateCode'=>'',
+				'placeOfPerformanceZIPCode'=>'',
+				'placeOfPerformanceCountryCode'=>'',
+				'mod_agency'=>'',
+				'maj_agency_cat'=>'',
+				'psc_cat'=>'',
+				'psc_sub'=>'',
+				'descriptionOfContractRequirement'=>'',
+				'PIID',
+				'complete_cat'=> array('c','o','p','n','a','f','u')
+				);
+		
+		$options['detail'] = $detail;
+		if($state != NULL) $options['state'] = $state;	
+		$options = array_intersect_key($options,array_merge(self::$base_options,$valid_options));
+			
+		return self::make_request($valid_options,'/fpds/fpds.php');	
+	}
+	
+	public function assistance($options,$detail=-1,$state=NULL){
+		$options['detail'] = $detail;
+		if($state != NULL) $options['state'] = $state;	
+		$valid_options = array('fiscal_year'=>'','fiscal_year_range'=>'','last_year_range'=>'','recipient_name'=>'','recipient_city_name'=>'','recipient_state_code'=>'','recipient_zip'=>'','recipient_county_name'=>'','recipient_cd'=>'','principal_place_state_code'=>'','principal_place_cc'=>'','agency_code'=>'','maj_agency_cat'=>'','recip_cat_type'=>array('f','g','h','i','n','o'), 'asst_cat_type'=>array('d','g','i','l','o'),'project_description'=>'','cfda_program_num'=>'','federal_award_id'=>'');
+		
+		$options = array_intersect_key($options,array_merge(self::$base_options,$valid_options));
+		
+		return self::make_request($valid_options,'/faads/faads.php');	
+	
+	}
+	
+	public function recover($options,$detail=-1){
+	//doesn't use 'sort' option ,it is 'sortp', remove and add value before doing array merges
+	
+	
+	$valid_options =array( 
+				'sortp'=> array('r','s','f','g','z','x','k','y','h','e','j','t','a','d'),
+				'recipient_name'=>'',
+				'entity_duns'=>'',
+				'recipient_st'=>''
+				'recipient_cd'=>'',
+				'recipient_zip_code'=>'',
+				'recipient_rl'=>array('p','s','v'),
+				'pop_state_cd'=>'',
+				'pop_cd'=>'',
+				'pop_postal_cd'=>'',
+				'funding_agency_cd'=>'',
+				'awarding_agency_cd'=>'',
+				'funding_tas'=>'',
+				'cfda_number'=>'',
+				'govt_contract_office_cd'=>'',
+				'award_type' => array('G','L','C'),
+				'award_number'=>'',
+				'order_number'=>'',
+				'activity_yn'=> array('y','n');
+				'project_description'=>'',
+				'full_text'=> '',
+				'award_amount'=>'',
+				'number_of_jobs'=>'',
+				'recipient_officer_totalcomp_1'=>
+				);
+	
+	$temp_array = self::$base_options;
+	unset($temp_array['sort']);
+	
+	$options = array_intersect_key($options,array_merge($temp_array,$valid_options));
+		
+		return self::make_request($valid_options,'/faads/faads.php');	
+	
+	
+	// rcv.php
+	
+	}
+}
