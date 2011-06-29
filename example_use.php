327354e04d19dfb234e0b3cfe906ca04db1c8261
Ronaldo Barbachano <ronaldo@redcapmedia.com>
2011-06-29 14:22:25 -0700

    Added test call to assistance and recover methods.

--- example_use.php
+++ example_use.php
@@ -8,5 +8,8 @@ $new = new fedSpendingApi();
 
 echo $new->contracts(array('state'=>'AL','recipient_name'=>'Smith'));
 
+echo $new->assistance(array('recipient_name'=>'Smith', 'fiscal_year'=>2006));
+
+echo $new->recover(array('recipient_st'=>'IA'));
 // Debug information
 die(print_r($new).print_r(get_object_vars($new)).print_r(get_class_methods(get_class($new))));
