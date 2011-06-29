00aba35afbd6ac4a3af5aafd2ad59f68e6446079
Ronaldo Barbachano <ronaldo@redcapmedia.com>
2011-06-29 12:30:09 -0700

    Added example contracts call.

--- example_use.php
+++ example_use.php
@@ -6,5 +6,7 @@ require 'fedSpendingApi.php';
 
 $new = new fedSpendingApi();
 
+echo $new->contracts(array('state'=>'AL','recipient_name'=>'Smith'));
+
 // Debug information
 die(print_r($new).print_r(get_object_vars($new)).print_r(get_class_methods(get_class($new))));
